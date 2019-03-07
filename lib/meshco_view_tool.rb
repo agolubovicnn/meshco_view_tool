41096b2266c58ffb0af79eb4b0923606d10a2ea6
Aleksandar GoluboviĆ <agolubovicnn@gmail.com>
2019-03-07 12:36:08 +0000

    Meshco tool initial commit

---
+++ lib/meshco_view_tool.rb
@@ -0,0 +1,7 @@
+require "meshco_view_tool/version"
+require "meshco_view_tool/renderer"
+
+module MeshcoViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/meshco_view_tool/version.rb
@@ -0,0 +1,3 @@
+module MeshcoViewTool
+  VERSION = "0.1.0"
+end
